d333b0dfd9c33628a77a2bae50f04d258c741b1e
0xKitsune <0xKitsune@protonmail.com>
2022-05-23 16:05:03 -0400

    updated structure to test setups

--- src/test/GasTest.t.sol
+++ src/test/GasTest.t.sol
@@ -5,29 +5,47 @@ import "../../lib/test.sol";
 import "../../lib/Console.sol";
 
 contract GasTest is DSTest {
-    GasReport gasReport;
+    Contract0 c0;
+    Contract1 c1;
+    Contract2 c2;
+    Contract3 c3;
+    Contract4 c4;
+    Contract5 c5;
+    Contract6 c6;
+    Contract7 c7;
 
     function setUp() public {
-        gasReport = new GasReport();
+        c0 = new Contract0();
+        c1 = new Contract1();
+        c2 = new Contract2();
+        c3 = new Contract3();
+        c4 = new Contract4();
+        c5 = new Contract5();
+        c6 = new Contract6();
+        c7 = new Contract7();
     }
 
     function testGas() public {
-        gasReport.addTest(100, 3434);
-        gasReport.addAssemblyTest(100, 3434);
-        gasReport.subTest(3434, 100);
-        gasReport.subAssemblyTest(3434, 100);
-        gasReport.mulTest(3434, 100);
-        gasReport.mulAssemblyTest(3434, 100);
-        gasReport.divTest(3434, 100);
-        gasReport.divAssemblyTest(3434, 100);
+        c0.addTest(100, 23498234);
+        c1.addAssemblyTest(100, 23498234);
+        c2.subTest(23498234, 100);
+        c3.subAssemblyTest(23498234, 100);
+        c4.mulTest(23498234, 100);
+        c5.mulAssemblyTest(23498234, 100);
+        c6.divTest(23498234, 100);
+        c7.divAssemblyTest(23498234, 100);
     }
 }
 
-contract GasReport {
+contract Contract0 {
+    //addition in Solidity
     function addTest(uint256 a, uint256 b) public pure {
         uint256 c = a + b;
     }
+}
 
+contract Contract1 {
+    //addition in assembly
     function addAssemblyTest(uint256 a, uint256 b) public pure {
         assembly {
             let c := add(a, b)
@@ -38,11 +56,17 @@ contract GasReport {
             }
         }
     }
+}
 
+contract Contract2 {
+    //subtraction in Solidity
     function subTest(uint256 a, uint256 b) public pure {
         uint256 c = a - b;
     }
+}
 
+contract Contract3 {
+    //subtraction in assembly
     function subAssemblyTest(uint256 a, uint256 b) public pure {
         assembly {
             let c := sub(a, b)
@@ -53,11 +77,17 @@ contract GasReport {
             }
         }
     }
+}
 
+contract Contract4 {
+    //multiplication in Solidity
     function mulTest(uint256 a, uint256 b) public pure {
         uint256 c = a * b;
     }
+}
 
+contract Contract5 {
+    //multiplication in assembly
     function mulAssemblyTest(uint256 a, uint256 b) public pure {
         assembly {
             let c := mul(a, b)
@@ -68,11 +98,17 @@ contract GasReport {
             }
         }
     }
+}
 
+contract Contract6 {
+    //division in Solidity
     function divTest(uint256 a, uint256 b) public pure {
         uint256 c = a * b;
     }
+}
 
+contract Contract7 {
+    //division in assembly
     function divAssemblyTest(uint256 a, uint256 b) public pure {
         assembly {
             let c := div(a, b)
