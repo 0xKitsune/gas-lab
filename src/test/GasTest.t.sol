210df9b1d7821aad9f787486902e571d2ace638c
0xKitsune <0xKitsune@protonmail.com>
2022-05-23 13:23:18 -0400

    updated gas lab to be one contract

--- src/test/GasTest.t.sol
+++ src/test/GasTest.t.sol
@@ -5,24 +5,82 @@ import "../../lib/test.sol";
 import "../../lib/Console.sol";
 
 contract GasTest is DSTest {
-    Optimized optimized;
-    Unoptimized unoptimized;
+    GasReport gasReport;
 
     function setUp() public {
-        unoptimized = new Unoptimized();
-        optimized = new Optimized();
+        gasReport = new GasReport();
     }
 
-    function testGasCost() public {
-        unoptimized.gasTest();
-        optimized.gasTest();
+    function testGas() public {
+        gasReport.addTest(100, 3434);
+        gasReport.addAssemblyTest(100, 3434);
+        gasReport.subTest(3434, 100);
+        gasReport.subAssemblyTest(3434, 100);
+        gasReport.mulTest(3434, 100);
+        gasReport.mulAssemblyTest(3434, 100);
+        gasReport.divTest(3434, 100);
+        gasReport.divAssemblyTest(3434, 100);
     }
 }
 
-contract Optimized {
-    function gasTest() external {}
-}
+contract GasReport {
+    function addTest(uint256 a, uint256 b) public pure {
+        uint256 c = a + b;
+    }
 
-contract Unoptimized {
-    function gasTest() external {}
+    function addAssemblyTest(uint256 a, uint256 b) public pure {
+        assembly {
+            let c := add(a, b)
+
+            if lt(c, a) {
+                mstore(0x00, "overflow")
+                revert(0x00, 0x20)
+            }
+        }
+    }
+
+    function subTest(uint256 a, uint256 b) public pure {
+        uint256 c = a - b;
+    }
+
+    function subAssemblyTest(uint256 a, uint256 b) public pure {
+        assembly {
+            let c := sub(a, b)
+
+            if gt(c, a) {
+                mstore(0x00, "underflow")
+                revert(0x00, 0x20)
+            }
+        }
+    }
+
+    function mulTest(uint256 a, uint256 b) public pure {
+        uint256 c = a * b;
+    }
+
+    function mulAssemblyTest(uint256 a, uint256 b) public pure {
+        assembly {
+            let c := mul(a, b)
+
+            if lt(c, a) {
+                mstore(0x00, "overflow")
+                revert(0x00, 0x20)
+            }
+        }
+    }
+
+    function divTest(uint256 a, uint256 b) public pure {
+        uint256 c = a * b;
+    }
+
+    function divAssemblyTest(uint256 a, uint256 b) public pure {
+        assembly {
+            let c := div(a, b)
+
+            if gt(c, a) {
+                mstore(0x00, "underflow")
+                revert(0x00, 0x20)
+            }
+        }
+    }
 }
