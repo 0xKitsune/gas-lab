33558436e84b19dea5c51aabd8be1c301b952bf2
0xKitsune <0xKitsune@protonmail.com>
2022-05-24 09:48:54 -0400

    added optimizer runs

--- src/test/GasTest.t.sol
+++ src/test/GasTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.13;
+pragma solidity >=0.8.14;
 
 import "../../lib/test.sol";
 import "../../lib/Console.sol";
@@ -7,116 +7,42 @@ import "../../lib/Console.sol";
 contract GasTest is DSTest {
     Contract0 c0;
     Contract1 c1;
-    Contract2 c2;
-    Contract3 c3;
-    Contract4 c4;
-    Contract5 c5;
-    Contract6 c6;
-    Contract7 c7;
 
     function setUp() public {
         c0 = new Contract0();
         c1 = new Contract1();
-        c2 = new Contract2();
-        c3 = new Contract3();
-        c4 = new Contract4();
-        c5 = new Contract5();
-        c6 = new Contract6();
-        c7 = new Contract7();
     }
 
-    function testGas() public {
-        c0.addTest(100, 23498234);
-        c1.addAssemblyTest(100, 23498234);
-        c2.subTest(23498234, 100);
-        c3.subAssemblyTest(23498234, 100);
-        c4.mulTest(23498234, 100);
-        c5.mulAssemblyTest(23498234, 100);
-        c6.divTest(23498234, 100);
-        c7.divAssemblyTest(23498234, 100);
+    function testGas() public view {
+        c0.mul2();
+        c1.shl2();
     }
 }
 
 contract Contract0 {
-    //addition in Solidity
-    function addTest(uint256 a, uint256 b) public pure {
-        uint256 c = a + b;
+    function mul2() public view {
+        uint256 val = 10;
+        uint256 valMulTwo = val * 2;
+        valMulTwo++;
     }
 }
 
 contract Contract1 {
-    //addition in assembly
-    function addAssemblyTest(uint256 a, uint256 b) public pure {
-        assembly {
-            let c := add(a, b)
-
-            if lt(c, a) {
-                mstore(0x00, "overflow")
-                revert(0x00, 0x20)
-            }
-        }
+    function shl2() public view {
+        uint256 val = 10;
+        uint256 valMulTwo = val << 1;
+        valMulTwo++;
     }
 }
 
-contract Contract2 {
-    //subtraction in Solidity
-    function subTest(uint256 a, uint256 b) public pure {
-        uint256 c = a - b;
-    }
-}
+contract Contract2 {}
 
-contract Contract3 {
-    //subtraction in assembly
-    function subAssemblyTest(uint256 a, uint256 b) public pure {
-        assembly {
-            let c := sub(a, b)
+contract Contract3 {}
 
-            if gt(c, a) {
-                mstore(0x00, "underflow")
-                revert(0x00, 0x20)
-            }
-        }
-    }
-}
+contract Contract4 {}
 
-contract Contract4 {
-    //multiplication in Solidity
-    function mulTest(uint256 a, uint256 b) public pure {
-        uint256 c = a * b;
-    }
-}
+contract Contract5 {}
 
-contract Contract5 {
-    //multiplication in assembly
-    function mulAssemblyTest(uint256 a, uint256 b) public pure {
-        assembly {
-            let c := mul(a, b)
+contract Contract6 {}
 
-            if lt(c, a) {
-                mstore(0x00, "overflow")
-                revert(0x00, 0x20)
-            }
-        }
-    }
-}
-
-contract Contract6 {
-    //division in Solidity
-    function divTest(uint256 a, uint256 b) public pure {
-        uint256 c = a * b;
-    }
-}
-
-contract Contract7 {
-    //division in assembly
-    function divAssemblyTest(uint256 a, uint256 b) public pure {
-        assembly {
-            let c := div(a, b)
-
-            if gt(c, a) {
-                mstore(0x00, "underflow")
-                revert(0x00, 0x20)
-            }
-        }
-    }
-}
+contract Contract7 {}
