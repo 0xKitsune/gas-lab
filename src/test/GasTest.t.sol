ee269bebb82801deaf027858d7209031b35c8d3a
0xKitsune <77890308+0xKitsune@users.noreply.github.com>
2022-05-30 21:05:48 -0400

    Update GasTest.t.sol

--- src/test/GasTest.t.sol
+++ src/test/GasTest.t.sol
@@ -7,39 +7,101 @@ import "../../lib/Console.sol";
 contract GasTest is DSTest {
     Contract0 c0;
     Contract1 c1;
+    Contract2 c2;
+    Contract3 c3;
+    Contract4 c4;
 
     function setUp() public {
         c0 = new Contract0();
         c1 = new Contract1();
+        c2 = new Contract2();
+        c3 = new Contract3();
+        c4 = new Contract4();
     }
 
-    function testGas() public view {
-        c0.mul2();
-        c1.shl2();
+    function testGas() public {
+        c0.iPlusPlus();
+        c1.plusPlusI();
+        c2.uncheckedPlusPlusI();
+        c3.safeUncheckedPlusPlusI();
+        c4.inlineAssemblyLoop();
     }
 }
 
 contract Contract0 {
-    function mul2() public view {
-        uint256 val = 10;
-        uint256 valMulTwo = val * 2;
-        valMulTwo++;
+    //loop with i++
+    function iPlusPlus() public pure {
+        uint256 j = 0;
+        for (uint256 i; i < 10; i++) {
+            j++;
+        }
     }
 }
 
 contract Contract1 {
-    function shl2() public view {
-        uint256 val = 10;
-        uint256 valMulTwo = val << 1;
-        valMulTwo++;
+    //loop with ++i
+    function plusPlusI() public pure {
+        uint256 j = 0;
+        for (uint256 i; i < 10; ++i) {
+            j++;
+        }
     }
 }
 
-contract Contract2 {}
+contract Contract2 {
+    //loop with unchecked{++i}
+    function uncheckedPlusPlusI() public pure {
+        uint256 j = 0;
+        for (uint256 i; i < 10; ) {
+            j++;
 
-contract Contract3 {}
+            unchecked {
+                ++i;
+            }
+        }
+    }
+}
 
-contract Contract4 {}
+contract Contract3 {
+    //loop with unchecked{++i} with additional overflow check
+    function safeUncheckedPlusPlusI() public pure {
+        uint256 j = 0;
+        uint256 i = 0;
+        for (i; i < 10; ) {
+            j++;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        //check for overflow
+        assembly {
+            if lt(i, 10) {
+                mstore(0x00, "loop overflow")
+                revert(0x00, 0x20)
+            }
+        }
+    }
+}
+
+contract Contract4 {
+    //loop with inline assembly
+    function inlineAssemblyLoop() public pure {
+        assembly {
+            let j := 0
+
+            for {
+                let i := 0
+            } lt(i, 10) {
+                i := add(i, 0x01)
+            } {
+                j := add(j, 0x01)
+            }
+        }
+    }
+}
+
 
 contract Contract5 {}
 
